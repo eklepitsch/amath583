78cde377850aaa2aa0943b32bfa8127e1b246a35
Evan Klepitsch <e.klepitsch@gmail.com>
2024-04-20 23:17:38 -0500

    Add mm_kij.

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -10,12 +10,14 @@
 #include <vector>
 
 template<typename T>
-long double multiply_square_matrices(std::size_t n, unsigned ntrials)
+std::pair<long double, long double> multiply_square_matrices(std::size_t n,
+                                                             unsigned ntrials)
 {
    auto start = std::chrono::high_resolution_clock::now();
    auto stop = std::chrono::high_resolution_clock::now();
-   long double elapsedtime = 0.L;
-   long double avgtime;
+   long double jki_elapsedtime = 0.L;
+   long double kij_elapsedtime = 0.L;
+   long double jki_avgtime, kij_avgtime;
 
    for(unsigned i=0; i<ntrials; ++i)
    {
@@ -28,11 +30,19 @@ long double multiply_square_matrices(std::size_t n, unsigned ntrials)
       stop = std::chrono::high_resolution_clock::now();
       auto duration =
          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
-      elapsedtime += (duration.count()*1.e-9); //Convert duration to seconds
+      jki_elapsedtime += (duration.count()*1.e-9); //Convert duration to seconds
+
+      start = std::chrono::high_resolution_clock::now();
+      mm_kij(static_cast<T>(1), *A, *B, static_cast<T>(1), *C, n, n, n);
+      stop = std::chrono::high_resolution_clock::now();
+      duration =
+         std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+      kij_elapsedtime += (duration.count()*1.e-9); //Convert duration to seconds
    }
 
-   avgtime = elapsedtime/static_cast<long double>(ntrials);
-   return avgtime;
+   jki_avgtime = jki_elapsedtime/static_cast<long double>(ntrials);
+   kij_avgtime = kij_elapsedtime/static_cast<long double>(ntrials);
+   return std::pair<long double, long double>(jki_avgtime, kij_avgtime);
 }
 
 void problem_1()
@@ -45,12 +55,19 @@ void problem_1()
    {
       // O(n^3), 3 because we have 2 multiplies and 1 addition
       auto flops = 3 * std::pow(n, 3);
+      auto result_float = multiply_square_matrices<float>(n, 5);
+      auto result_double = multiply_square_matrices<double>(n, 5);
+      // first = jki, second = kij
       float_results << std::fixed << std::setprecision(0) << n << ", " <<
          flops << ", " << std::scientific << std::setprecision(10) <<
-         multiply_square_matrices<float>(n, 5) << std::endl;
+         result_float.first << ", " << flops / result_float.first << ", " <<
+         result_float.second << ", " << flops / result_float.second <<
+         std::endl;
       double_results << std::fixed << std::setprecision(0) << n << ", " <<
          flops << ", " << std::scientific << std::setprecision(10) <<
-         multiply_square_matrices<double>(n, 5) << std::endl;
+         result_double.first << ", " << flops / result_double.first << ", " <<
+         result_double.second << ", " << flops / result_double.second <<
+         std::endl;
    }
 }
 
