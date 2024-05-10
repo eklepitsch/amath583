c34426c5a86db1e9932ed366791f3e9660d6b3dc
Evan Klepitsch <e.klepitsch@gmail.com>
2024-05-09 19:12:26 -0500

    Finish up problem 6.

--- hw4/hw4.cpp
+++ hw4/hw4.cpp
@@ -219,8 +219,18 @@ double riemann_sum_cxx_threads(std::function<double(double)> f, double xi,
    threads.reserve(nthreads);
    for(auto i = 0; i<nthreads; ++i)
    {
-      threads.emplace_back(riemann, f, xi + i * dx * points_per_thread, dx, points_per_thread,
-                           std::ref(sum_mutex), std::ref(sum));
+      if(i == nthreads - 1)
+      {
+         // The last thread gets the leftovers
+         threads.emplace_back(riemann, f, xi + i * dx * points_per_thread, dx,
+                              points_per_thread + leftover_points,
+                              std::ref(sum_mutex), std::ref(sum));
+      }
+      else
+      {
+         threads.emplace_back(riemann, f, xi + i * dx * points_per_thread, dx,
+                              points_per_thread, std::ref(sum_mutex), std::ref(sum));
+      }
    }
    for(auto i = 0; i<nthreads; ++i)
    {
@@ -229,11 +239,39 @@ double riemann_sum_cxx_threads(std::function<double(double)> f, double xi,
    return sum;
 }
 
+static const double EXACT_SOLUTION_PROBLEM_6 = \
+   ((double)35/(double)8) + std::log((double)6);
+
 void problem_6()
 {
-   auto sum = riemann_sum_cxx_threads(f_x, 1, 6, 1000000, 10);
+   std::ofstream scaling_results;
+   scaling_results.open("./artifacts/scaling-results.csv");
+   std::ofstream error_results;
+   error_results.open("./artifacts/error-results.csv");
+
+   unsigned n = 1;
+   while(n <= 16)
+   {
+      auto start= std::chrono::high_resolution_clock::now();
+      auto sum = riemann_sum_cxx_threads(f_x, 1, 6, 1E8, n);
+      auto stop = std::chrono::high_resolution_clock::now();
+      auto duration =
+         std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+
+      scaling_results << std::fixed << std::setprecision(0) << n << ", " <<
+         std::scientific << std::setprecision(10) << 
+         std::fabs(sum - EXACT_SOLUTION_PROBLEM_6) << ", " <<
+         sum << ", " << duration.count()*1.e-9 << std::endl;
+      
+      n <<= 1;
+   }
+
+   auto sum = riemann_sum_cxx_threads(f_x, 1, 6, 1000000, 9);
    //auto sum = riemann_sum_cxx_threads(x_squared, 0, 1, 1000000, 10);
    std::cout << "Rieman sum: " << std::setprecision(10) << sum << std::endl;
+
+   scaling_results.close();
+   error_results.close();
 }
 
 int main()
