fe946a91286be92821d6b7e8668ea684fb27e1ea
Evan Klepitsch <e.klepitsch@gmail.com>
2024-05-14 20:32:40 -0500

    HW4, Problem 7 - initial run with Hyak

--- hw4/hw4_mpi.cpp
+++ hw4/hw4_mpi.cpp
@@ -1,10 +1,11 @@
 
-#include<algorithm>
+#include <algorithm>
 #include <chrono>
 #include <cmath>
-#include<cstdio>
-#include<cstdlib>
-#include<ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 #include <fstream>
 #include <functional>
 #include <iomanip>
@@ -12,8 +13,8 @@
 #include <mpi.h>
 #include <mutex>
 #include <thread>
-#include<utility>
-#include<vector>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
@@ -45,8 +46,8 @@ double riemann(std::function<double(double)> f, double xi, double dx,
    return sum;
 }
 
-double riemann_sum_mpi(std::function<double(double)> f, double xi,
-                       double xf, unsigned npoints, int rank, int nthreads)
+double riemann(std::function<double(double)> f, double xi,
+               double xf, unsigned npoints, int rank, int nthreads)
 {
    unsigned points_per_thread = npoints / nthreads;
    unsigned leftover_points = npoints % nthreads;
@@ -64,22 +65,60 @@ double riemann_sum_mpi(std::function<double(double)> f, double xi,
    return global_sum;
 }
 
-int main()
+static const double EXACT_SOLUTION_PROBLEM_6 = \
+   ((double)35/(double)8) + std::log((double)6);
+
+double riemann_sum_mpi(std::function<double(double)> f, double xi, double xf,
+                       double npoints)
 {
-   //hello_world();
+   double retVal = 0;
    MPI_Init(nullptr, nullptr);
+
    int rank, size;
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-   double sum = riemann_sum_mpi(f_x, 1, 6, 1E9, rank, size);
-   //double sum = riemann_sum_mpi(x_squared, 0, 1, 1E6, rank, size);
+   double sum = riemann(f_x, 1, 6, npoints, rank, size);
+   //double sum = riemann(x_squared, 0, 1, 1E6, rank, size);
 
    if(rank == 0)
    {
-      cout << "Rieman sum: " << std::setprecision(10) << sum << endl;
+      cout << "Rieman sum (" << size << " threads): " <<
+         std::setprecision(10) << sum << endl;
+
+      double error = std::fabs(sum - EXACT_SOLUTION_PROBLEM_6);
+      std::cout << std::setprecision(0) << "log(error) (" << npoints << " points): "
+         << std::setprecision(10) << std::log10(error) << std::endl;
+
+      retVal = sum;
    }
 
    MPI_Finalize();
+   return retVal;
+}
+
+int main(int argc, char **argv)
+{
+   if(!std::strcmp(argv[1], "a"))
+   {
+      auto sum = riemann_sum_mpi(f_x, 1, 6, 1E8);
+   }
+   else if(!std::strcmp(argv[1], "b"))
+   {
+      int npoints = std::stoi(std::string(argv[2]));
+      if(npoints > 0 && npoints <= 1.e9)
+      {
+         auto sum = riemann_sum_mpi(f_x, 1, 6, npoints);
+      }
+      else
+      {
+         std::cout << "Invalid number of partition points" << std::endl;
+      }
+   }
+   else
+   {
+      std::cout << "Invalid option. Use 'a' for 7a and 'b' for 7b." << std::endl;
+   }
+
    return 0;
 }
