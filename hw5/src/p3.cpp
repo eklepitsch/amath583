645bdb8d11021df8a6b5cea77f0cb0a4928dbc63
Evan Klepitsch <e.klepitsch@gmail.com>
2024-05-18 16:10:41 -0500

    Remove wrappers

--- hw5/src/p3.cpp
+++ hw5/src/p3.cpp
@@ -7,27 +7,6 @@
 
 using namespace std;
 
-void wrap_cblas_daxpy(const int N, const double alpha, const double* X,
-                      const int incX, double* Y, const int incY)
-{
-   cblas_daxpy(N, alpha, X, incX, Y, incY);
-}
-
-void wrap_cblas_dgemv(const enum CBLAS_ORDER order, const enum CBLAS_TRANSPOSE TransA,
-                      const int M, const int N, const double alpha, const double* A, const int lda,
-                      const double* X, const int incX, const double beta, double* Y, const int incY)
-{
-   cblas_dgemv(order, TransA, M, N, alpha, A, lda, X, incX, beta, Y, incY);
-}
-
-void wrap_cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
-                      const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K,
-                      const double alpha, const double* A, const int lda, const double* B,
-                      const int ldb, const double beta, double* C, const int ldc)
-{
-   cblas_dgemm(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc);
-}
-
 int main(int argc, char** argv)
 {
    ofstream results("./artifacts/p3.csv");
@@ -55,14 +34,14 @@ int main(int argc, char** argv)
          // Level 1 BLAS
          double alpha = 2.0;
          auto start = std::chrono::high_resolution_clock::now();
-         wrap_cblas_daxpy(n, alpha, x->data(), 1, y->data(), 1);
+         cblas_daxpy(n, alpha, x->data(), 1, y->data(), 1);
          auto stop = std::chrono::high_resolution_clock::now();
          l1_elapsed += std::chrono::duration_cast<std::chrono::nanoseconds>(
                stop - start).count()*1.e-9;
 
          // Level 2 BLAS
          start = std::chrono::high_resolution_clock::now();
-         wrap_cblas_dgemv(CblasColMajor, CblasNoTrans, n, n, 2.0, A->data(), n,
+         cblas_dgemv(CblasColMajor, CblasNoTrans, n, n, 2.0, A->data(), n,
                           x->data(), 1, 2.0, y->data(), 1);
          stop = std::chrono::high_resolution_clock::now();
          l2_elapsed += std::chrono::duration_cast<std::chrono::nanoseconds>(
@@ -70,7 +49,7 @@ int main(int argc, char** argv)
 
          // Level 3 BLAS
          start = std::chrono::high_resolution_clock::now();
-         wrap_cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 2.0,
+         cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 2.0,
                           A->data(), n, B->data(), n, 2.0, C->data(), n);
          stop = std::chrono::high_resolution_clock::now();
          l3_elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(
