ec420cd0ce800252501d0871d98ac6ef570658a2
Evan Klepitsch <e.klepitsch@gmail.com>
2024-05-24 18:27:55 -0500

    Remove commented out code from p4.cpp

--- hw5/src/p4.cpp
+++ hw5/src/p4.cpp
@@ -1,5 +1,5 @@
 #include "matrix-utils.hpp"
-// #include <cblas.h>
+#include <cblas.h>
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 #include <chrono>
@@ -9,38 +9,38 @@
 
 using namespace std;
 
-// void measure_openblas_gemm(unsigned long long n, unsigned ntrials, ofstream& results)
-// {
-//    long double elapsed = 0.0;
-//    long double avgtime = 0.0;
+void measure_openblas_gemm(unsigned long long n, unsigned ntrials, ofstream& results)
+{
+   long double elapsed = 0.0;
+   long double avgtime = 0.0;
 
-//    auto A = GenerateSquareMatrix<double>(n);
-//    auto B = GenerateSquareMatrix<double>(n);
-//    auto C = GenerateSquareMatrix<double>(n);
+   auto A = GenerateSquareMatrix<double>(n);
+   auto B = GenerateSquareMatrix<double>(n);
+   auto C = GenerateSquareMatrix<double>(n);
 
-//    for(auto i=0; i<ntrials; ++i)
-//    {
-//       auto start = std::chrono::high_resolution_clock::now();
-//       cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 2.0,
-//                      A->data(), n, B->data(), n, 2.0, C->data(), n);
-//       auto stop = std::chrono::high_resolution_clock::now();
-//       elapsed += std::chrono::duration_cast<std::chrono::nanoseconds>(
-//             stop - start).count()*1.e-9;
-//    }
-//    avgtime = elapsed/ntrials;
+   for(auto i=0; i<ntrials; ++i)
+   {
+      auto start = std::chrono::high_resolution_clock::now();
+      cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 2.0,
+                     A->data(), n, B->data(), n, 2.0, C->data(), n);
+      auto stop = std::chrono::high_resolution_clock::now();
+      elapsed += std::chrono::duration_cast<std::chrono::nanoseconds>(
+            stop - start).count()*1.e-9;
+   }
+   avgtime = elapsed/ntrials;
 
-//    // dgemm flop count = 2mnk + 3mn, = 2n^3 + 3n^2 when m=n=k
-//    unsigned long long l3_flop = 2*n*n*n + 3*n*n;
+   // dgemm flop count = 2mnk + 3mn, = 2n^3 + 3n^2 when m=n=k
+   unsigned long long l3_flop = 2*n*n*n + 3*n*n;
 
-//    results << n << ", " << std::setprecision(10)
-//       << l3_flop << ", "
-//       << avgtime << ", "
-//       << (double)l3_flop/avgtime << ", "
-//       << ((double)l3_flop/avgtime)/1.e9
-//       << endl;
-   
-//    return;
-// }
+   results << n << ", " << std::setprecision(10)
+      << l3_flop << ", "
+      << avgtime << ", "
+      << (double)l3_flop/avgtime << ", "
+      << ((double)l3_flop/avgtime)/1.e9
+      << endl;
+
+   return;
+}
 
 void measure_cublas_gemm(unsigned long long n, unsigned ntrials,
                          ofstream& results, cublasHandle_t& hndl)
@@ -131,7 +131,7 @@ int main(int argc, char** argv)
    for(unsigned long long n=16; n<=max_dim; n*=2)
    {
       unsigned ntrials = 5;
-      // measure_openblas_gemm(n, ntrials, openblas_results);
+      measure_openblas_gemm(n, ntrials, openblas_results);
       measure_cublas_gemm(n, ntrials, cublas_results, hndl);
    }
 
