4f151d636dc7aa5ee97fa5815f1ee1c1ce808d1c
Evan Klepitsch <e.klepitsch@gmail.com>
2024-05-30 20:51:25 -0500

    HW6, P3

--- hw6/build.sh
+++ hw6/build.sh
@@ -15,5 +15,10 @@ set -x
 # g++ -g -std=c++17 -o $BUILD_DIR/xhw6_p1 $SRC_DIR/p1.cpp -llapacke -lblas
 
 # Problem 2
-nvcc -c -O3 -I$INCLUDE_DIR -arch sm_50 -o $BUILD_DIR/p2.o $SRC_DIR/p2.cpp
-g++ -g -o $BUILD_DIR/xhw6_p2 $BUILD_DIR/p2.o -lcudart -lm
+# nvcc -c -O3 -I$INCLUDE_DIR -arch sm_50 -o $BUILD_DIR/p2.o $SRC_DIR/p2.cpp
+# g++ -g -o $BUILD_DIR/xhw6_p2 $BUILD_DIR/p2.o -lcudart -lm
+
+# Problem 3
+cp $SRC_DIR/gradient-fftw.cpp $SRC_DIR/gradient-fftw.cu
+nvcc -c -arch sm_50 -std=c++14 -I$INCLUDE_DIR -I/usr/local/include -I/usr/local/cuda-12.1/include $SRC_DIR/gradient-fftw.cpp
+nvcc -o $BUILD_DIR/xgrad-fftw gradient-fftw.o -L/usr/local/lib -lfftw3 -L/usr/local/cuda-12.1/lib64 -lcudart -lcufft -lm
