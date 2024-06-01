8941c279e15389a9db0e530c4039216ad80ba908
Evan Klepitsch <e.klepitsch@gmail.com>
2024-06-01 11:29:34 -0500

    Move starter code for P3 to src directory.

--- exam2/build.sh
+++ exam2/build.sh
@@ -8,4 +8,7 @@ mkdir -p $BUILD_DIR
 set -x
 
 # Problem 1
-g++ -g -std=c++17 -o $BUILD_DIR/xexam2_p1 -I$SRC_DIR $SRC_DIR/mysparse.cpp
+g++ -g -std=c++14 -o $BUILD_DIR/xexam2_p1 -I$SRC_DIR $SRC_DIR/mysparse.cpp
+
+# Problem 3
+g++ -g -std=c++14 -o $BUILD_DIR/xexam2_p3 -I$SRC_DIR $SRC_DIR/match_pattern.cpp
